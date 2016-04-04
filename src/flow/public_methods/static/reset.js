169e2c63a33b37691e6f0f6f8ed735d11ef79ee2
Sam Hagman <samuel.hagman@gmail.com>
2016-04-04 14:19:14 -0400

    Repurpose Flow#reset to just always rollback to first step

--- src/flow/public_methods/static/reset.js
+++ src/flow/public_methods/static/reset.js
@@ -1,53 +1,15 @@
-const _ = require('lodash');
-const kue = require('kue');
-const Promise = require('bluebird');
 
 /**
- * Reset an active Flow instance back to a certain step
+ * Completely reset a flow so that it starts all over again from the originally provided data.
  * @method Flow.reset
  * @public
  * @param {object} _d - Object holding private Flow class data
  * @param {string} UUID - The UUID of a flow
- * @param {number} stepNumber - The step number to reset to
  * @returns {Promise}
  */
-function reset(_d, UUID, stepNumber) {
+function reset(_d, UUID) {
 
-    return _d.Flow.status(UUID)
-        .then(flowData => {
-            return new Promise((resolve, reject) => {
-
-                // Build mongoose update object
-                const updateObject = {};
-                updateObject.stepsTaken = stepNumber - 1;
-                updateObject.substepsTaken = [];
-                updateObject.phase = 'NoPhase';
-                updateObject.ancestors = _.omitBy(flowData.ancestors, (value, key) => parseInt(key, 10) >= flowData.stepsTaken);
-
-                // Update the flow's data in mongo
-                this.FlowModel.update({ _id: UUID }, updateObject, (err, value) => {
-                    if (err) { reject(err); }
-                    resolve(value);
-                });
-
-            });
-        })
-        .then(flowData => {
-            return new Promise((resolve, reject) => {
-
-                // Find the job in Kue and restart it
-                kue.Job.get(flowData.jobId, flowData.type, (err, job) => {
-                    if (err) { reject(err); }
-
-                    // Restarts the job
-                    job.inactive();
-                    resolve();
-                });
-            });
-        })
-        .catch(err => {
-            _d.Logger.error(`Error resetting flow ${UUID}: \n ${err.stack}`);
-        });
+    return _d.Flow.rollback(UUID, -1);
 }
 
 export default reset;
