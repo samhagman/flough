859697fe780952e81779ad5324578ae57af3b3d9
Sam Hagman <samuel.hagman@gmail.com>
2016-04-04 15:31:38 -0400

    Rename Flow#begin to Flow#beginChain

--- src/flow/public_methods/instance/begin.js
+++ src/flow/public_methods/instance/begin.js
@@ -2,7 +2,7 @@ const Promise = require('bluebird');
 const _ = require('lodash');
 
 /**
- * Initializes the Flow, needed to finish construction of Flow instance
+ * Initializes a Flow chain
  * @method Flow#beginChain
  * @public
  * @this Flow
@@ -10,12 +10,10 @@ const _ = require('lodash');
  * @param {Promise[]} [promiseArray=[]] - Array of promises to resolve before first job of flow will run, not necessarily before the .beginChain() will run.
  * @returns {Flow}
  */
-function begin(_d, promiseArray = []) {
+function beginChain(_d, promiseArray = []) {
 
     const _this = this;
     const Logger = _d.Logger;
-
-    Logger.info(`${_this.loggerPrefix} - START FLOW`);
 
     // Attach User passed promises to resolve before any flow.job()s run.
     _this.promised[ '0' ].concat(promiseArray);
@@ -44,39 +42,6 @@ function begin(_d, promiseArray = []) {
                             reject(err);
                         }
 
-                        // The passed _id wasn't found, this is a new Flow
-                        else if (!flowDoc) {
-
-                            //Logger.info(`${_this.loggerPrefix} Creating new Flow in Mongo...`);
-                            _d.FlowModel.create(
-                                {
-                                    _id:           _this.uuid,
-                                    type:          _this.type,
-                                    jobId:         _this.jobId,
-                                    stepsTaken:    _this.stepsTaken,
-                                    substepsTaken: _this.substepsTaken,
-                                    jobData:       _this.data,
-                                    isParent:      true,
-
-                                    // Reinitialize with related jobs if this is a helper flow
-                                    ancestors: _this.data._ancestors || {},
-                                    logs:      [],
-                                    childLogs: []
-                                })
-                                .then((flowDoc, err) => {
-                                    if (err) {
-                                        Logger.error(err.stack);
-                                        reject(err);
-                                    }
-                                    else {
-                                        //Logger.debug('Correctly made mongo doc');
-                                        //Logger.info(`[${_this.uuid}] New Flow created. Flow.start() complete.`);
-                                        resolve(_this);
-                                    }
-                                })
-                            ;
-                        }
-
                         // Found the _id in Mongo, we are restarting a failed Flow
                         else if (flowDoc) {
 
@@ -89,23 +54,23 @@ function begin(_d, promiseArray = []) {
                             resolve(_this);
                         }
                         else {
-                            reject(new Error(`[${_this.uuid}] Something went very very wrong when start()ing Flow...`));
+                            return reject(new Error(`[${_this.uuid}] Something went very very wrong when beginning Flow...`));
                         }
                     })
                 ;
             }
             else {
-                reject(new Error(`[${_this.uuid}] uuid passed to Flow.start() is not a valid ObjectId.`));
+                return reject(new Error(`[${_this.uuid}] uuid passed to Flow.start() is not a valid ObjectId.`));
             }
 
         } catch (err) {
             Logger.error(err.stack);
 
-            reject(err);
+            return reject(err);
         }
     }));
 
     return _this;
 }
 
-export default begin;
+export default beginChain;
