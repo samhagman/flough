df65f1c3e59e3fb09366b3538620bdf5c795b655
Sam Hagman <samuel.hagman@gmail.com>
2016-04-04 14:17:07 -0400

    Update Flow#reset to be called Flow#rollback
    This name change is intended to make the functionality of the old reset
    code more clear.

---
+++ src/flow/public_methods/static/rollback.js
@@ -0,0 +1,59 @@
+const _ = require('lodash');
+const kue = require('kue');
+const Promise = require('bluebird');
+
+/**
+ * Reset an active Flow instance back to a certain step
+ * @method Flow.rollback
+ * @memberOf Flow
+ * @alias Flow.rollback
+ * @public
+ * @param {object} _d - Object holding private Flow class data
+ * @param {string} UUID - The UUID of a flow
+ * @param {number} stepNumber - The step number to rollback to
+ * @returns {Promise}
+ */
+function rollback(_d, UUID, stepNumber) {
+
+    return _d.Flow.status(UUID)
+        .then(flowData => {
+            return new Promise((resolve, reject) => {
+
+                // Build mongoose update object
+                const updateObject = {};
+                updateObject.stepsTaken = stepNumber - 1;
+                updateObject.substepsTaken = [];
+                updateObject.phase = 'NoPhase';
+                updateObject.ancestors = _.omitBy(flowData.ancestors, (value, key) => flowData.stepsTaken <= parseInt(key, 10));
+
+                // Update the flow's data in mongo
+                this.FlowModel.update({ _id: UUID }, updateObject, (err, value) => {
+                    if (err) reject(err);
+
+                    resolve(value);
+                });
+
+            });
+        })
+        .then(flowData => {
+            return new Promise((resolve, reject) => {
+
+                // Find the job in Kue and restart it
+                kue.Job.get(flowData.jobId, flowData.type, (err, job) => {
+                    if (err) reject(err);
+
+                    // Remove the instance from memory if stored there
+                    if (_d.flowInstances.has(UUID)) _d.flowInstances.remove(UUID);
+
+                    // Restarts the job
+                    job.inactive();
+                    resolve();
+                });
+            });
+        })
+        .catch(err => {
+            _d.Logger.error(`Error resetting flow ${UUID}: ${ err.stack}`);
+        });
+}
+
+export default rollback;
