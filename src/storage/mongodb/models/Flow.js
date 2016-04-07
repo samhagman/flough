9a60601e9648d416d5f622cfa94bc6ffc165dc36
Sam Hagman <samuel.hagman@gmail.com>
2016-04-07 17:52:05 -0400

    Add `isStarted` field to Flow model

--- src/storage/mongodb/models/Flow.js
+++ src/storage/mongodb/models/Flow.js
@@ -32,6 +32,7 @@ export default function FlowSchemaBuilder(mongoose) {
             isParent:      { type: Boolean, required: true, default: false },
             isCancelled:   { type: Boolean, required: true, default: false },
             isCompleted:   { type: Boolean, required: true, default: false },
+            isRestarted:   { type: Boolean, required: true, default: false },
             result:        { type: Schema.Types.Mixed, default: null, required: false }
         },
         { collection: 'flow' });
