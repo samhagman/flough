41d3f3daea871601e9bd86883fad914256395022
Sam Hagman <samuel.hagman@gmail.com>
2016-04-27 17:02:31 -0400

    Update whitespace indentation

--- src/flow/private_methods/setFlowResult.js
+++ src/flow/private_methods/setFlowResult.js
@@ -4,7 +4,7 @@ const Promise = require('bluebird');
  * Set the result of an instance of Flow
  * @memberOf Flow
  * @protected
- * @param {Flow~privateData} _d - Private Flow data
+ * @param {PrivateFlowData} _d - Private Flow data
  * @param {Flow} flowInstance - The instance of Flow to act upon
  * @param {*} result - The result of the child flow
  * @returns {Promise.<*>}
@@ -13,22 +13,22 @@ function setFlowResult(_d, flowInstance, result) {
 
     return new Promise((resolve, reject) => {
         _d.FlowModel.findByIdAndUpdate(flowInstance.uuid, {
-                isCompleted: true,
-                result:      result
-            }, { new: true })
-            .then((flowDoc, err) => {
-                if (err) {
-                    _d.Logger.error(`[${flowInstance.kueJob.type}][${flowInstance.uuid}] Error updating complete flow in MongoDB. \n
+              isCompleted: true,
+              result:      result
+          }, { new: true })
+          .then((flowDoc, err) => {
+              if (err) {
+                  _d.Logger.error(`[${flowInstance.kueJob.type}][${flowInstance.uuid}] Error updating complete flow in MongoDB. \n
                                         $set complete => true \n\n
                                         $set result => ${JSON.stringify(result)}`);
-                    _d.Logger.error(`[ ${flowInstance.uuid} ] ${err.stack}`);
-                    return reject(err);
-                }
-                else {
-                    flowInstance.isCompleted = true;
-                    return resolve(result);
-                }
-            })
+                  _d.Logger.error(`[ ${flowInstance.uuid} ] ${err.stack}`);
+                  return reject(err);
+              }
+              else {
+                  flowInstance.isCompleted = true;
+                  return resolve(result);
+              }
+          })
         ;
     });
 }
