b1b008ab3bf9f2408099629fb5f8812648ed5cd8
Sam Hagman <samuel.hagman@gmail.com>
2016-04-27 17:05:36 -0400

    Change Flow#search to use Joi for parameter validation

--- src/flow/public_methods/static/search.js
+++ src/flow/public_methods/static/search.js
@@ -2,66 +2,65 @@ const reds = require('reds');
 const Promise = require('bluebird');
 const kue = require('kue');
 const _ = require('lodash');
+const Joi = require('joi');
 
 /**
  * Search for flows using MongoDB as the source of truth.
- * Results must match ALL specified parameters: jobIds, flowUUIDs, types
+ * Results must match ALL specified parameters: jobId, UUID, type
  * @method Flow.search
  * @public
  * @param {Flow~privateData} _d - Private Flow data
- * @param {Array} [jobIds] - Array of Kue job ids to match
- * @param {Array} [flowUUIDs] - Array of Flough flow UUIDs to match
- * @param {Array} [types] - Array of flow types to match
+ * @param {Array} [jobId] - Array of Kue job ids to match
+ * @param {Array} [UUID] - Array of Flough flow UUIDs to match
+ * @param {Array} [type] - Array of flow type to match
  * @param {string} [isCompleted] - Whether or not to only return isCompleted flows
  * @param {string} [isCancelled] - If set, will return only either cancelled or not cancelled flows. If not set, both.
- * @param {boolean} [activeJobs] - Whether or not to return only active Kue jobs
+ * @param {boolean} [kueActive] - Whether or not to return only active Kue jobs
  * @returns {Promise.<object[]>}
  */
-export default function search(_d, { jobIds, flowUUIDs, types, isCompleted, isCancelled, activeJobs }) {
+export default function search(_d, { jobId, UUID, type, isCompleted, isCancelled, kueActive }) {
 
-    const Logger = _d.Logger;
-    const flowModel = _d.FlowModel;
-
+    const { Logger, flowModel } = _d;
 
     return new Promise((resolve, reject) => {
 
-        if (flowUUIDs && !_.isArray(flowUUIDs)) {
-            reject('flowUUIDs must be an array');
-        }
+        const searchSchema = Joi.object().keys({
+            jobId:       Joi.array().items(Joi.number()).single(),
+            UUID:        Joi.array().items(Joi.string()).single(),
+            type:        Joi.array().items(Joi.string()).single(),
+            isCompleted: Joi.bool(),
+            isCancelled: Joi.bool(),
+            kueActive:   Joi.bool()
+        });
 
-        if (jobIds && !_.isArray(jobIds)) {
-            reject('jobIds must be an array');
-        }
+        const validResult = Joi.validate([ ...arguments ][ 1 ], searchSchema, { abortEarly: false, convert: false });
 
-        if (types && !_.isArray(types)) {
-            reject('types must be an array');
+        if (validResult.error) {
+            Logger.error('Error with Flow.search: \n' + validResult.error);
+            return reject(new Error('Invalid search parameters for Flow.search'));
         }
 
         // MongoDB Search Object
         let searchOptions = {};
 
-        if (_.isBoolean(isCompleted)) {
+        if (isCompleted !== undefined) {
             searchOptions.isCompleted = isCompleted;
         }
 
-        if (_.isBoolean(isCancelled)) {
+        if (isCancelled !== undefined) {
             searchOptions.isCancelled = isCancelled;
         }
 
-        if (_.isBoolean(activeJobs)) {
-            searchOptions.activeJobs = activeJobs;
+        if (UUID) {
+            searchOptions[ 'data._uuid' ] = { $in: castToArray(UUID) };
         }
 
-        if (flowUUIDs && flowUUIDs.length !== 0) {
-            searchOptions[ 'data._uuid' ] = { $in: flowUUIDs };
+        if (jobId) {
+            searchOptions.jobId = { $in: castToArray(jobId) };
         }
 
-        if (jobIds && jobIds.length !== 0) {
-            searchOptions.jobId = { $in: jobIds };
-        }
-
-        if (types && types.length !== 0) {
-            searchOptions.type = { $in: types };
+        if (type) {
+            searchOptions.type = { $in: castToArray(type) };
         }
 
         flowModel.find(searchOptions, (err, flows) => {
@@ -71,7 +70,7 @@ export default function search(_d, { jobIds, flowUUIDs, types, isCompleted, isCa
             }
             else {
                 // If they only want to return active jobs (those found in Kue) then filter out inactive jobs
-                if (activeJobs) {
+                if (kueActive) {
                     // Build promise array whose items resolve whether or not the job at the corresponding index in
                     // the jobs returned from MongoDB array (flows) is found inside Kue or not.
                     const promArray = flows.map((flow, index) => new Promise((resolve, reject) => {
@@ -90,9 +89,9 @@ export default function search(_d, { jobIds, flowUUIDs, types, isCompleted, isCa
                     // After we've checked active state of all jobs returned from MongoDB, filter out jobs that were
                     // not found in Kue and resolve the resulting array
                     Promise.all(promArray)
-                        .then(isActiveJobArray => {
-                            resolve(flows.filter((kueJob, index) => isActiveJobArray[ index ]));
-                        })
+                           .then(isActiveJobArray => {
+                               resolve(flows.filter((kueJob, index) => isActiveJobArray[ index ]));
+                           })
                     ;
                 }
                 else {
@@ -103,3 +102,14 @@ export default function search(_d, { jobIds, flowUUIDs, types, isCompleted, isCa
         ;
     });
 };
+
+function castToArray(value) {
+    if (value) {
+        return _.isArray(value)
+            ? value
+            : [ value ];
+    }
+    else {
+        return [];
+    }
+}
