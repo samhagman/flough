08447534faa38afdc70bbb64a48d2b10b5738ce9
Sam Hagman <samuel.hagman@gmail.com>
2016-04-29 17:28:15 -0400

    Update Flow~setFlowResult to correctly handle returned Mongoose promise

--- src/flow/private_methods/setFlowResult.js
+++ src/flow/private_methods/setFlowResult.js
@@ -16,18 +16,22 @@ function setFlowResult(_d, flowInstance, result) {
               isCompleted: true,
               result:      result
           }, { new: true })
-          .then((flowDoc, err) => {
-              if (err) {
-                  _d.Logger.error(`[${flowInstance.kueJob.type}][${flowInstance.uuid}] Error updating complete flow in MongoDB. \n
-                                        $set complete => true \n\n
-                                        $set result => ${JSON.stringify(result)}`);
-                  _d.Logger.error(`[ ${flowInstance.uuid} ] ${err.stack}`);
-                  return reject(err);
-              }
-              else {
+          .then((flowDoc) => {
+              if (flowDoc) {
                   flowInstance.isCompleted = true;
                   return resolve(result);
               }
+              else {
+                  return reject(new Error(`Something went very wrong with setFlowResult for ${flowInstance.uuid}`));
+              }
+
+          })
+          .catch(err => {
+              _d.Logger.error(`[${flowInstance.kueJob.type}][${flowInstance.uuid}] Error updating complete flow in MongoDB. \n
+                                        $set complete => true \n\n
+                                        $set result => ${JSON.stringify(result)}`);
+              _d.Logger.error(`[ ${flowInstance.uuid} ] ${err.stack}`);
+              return reject(err);
           })
         ;
     });
