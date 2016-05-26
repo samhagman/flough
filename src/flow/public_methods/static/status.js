dc5eaa1367baa43b21a0816b72cbd2480104b9db
Sam Hagman <samuel.hagman@gmail.com>
2016-05-26 15:38:51 -0400

    Update Flow.status to use new Flow.search format

--- src/flow/public_methods/static/status.js
+++ src/flow/public_methods/static/status.js
@@ -1,4 +1,3 @@
-
 /**
  * TODO - Make this return something more useful than just searching for the flow
  * @method Flow.status
@@ -11,7 +10,7 @@
 function status(_d, UUID) {
 
     // search for the flow by UUID and return it
-    return _d.Flow.search(UUID);
+    return _d.Flow.search({ UUID });
 }
 
 export default status;
